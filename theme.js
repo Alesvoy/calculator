6404719771e3481d6648d5ba315c884526c0c408
Alesvoy <alesvoy@gmail.com>
2022-06-01 14:24:36 -0500

    Fixed bug with button underlay button

--- theme.js
+++ theme.js
@@ -1,6 +1,6 @@
 // DONE: Select checkboxes and only allow for one to be active.
 // DONE: Be able to see which checkbox is active.
-// TODO: Change the theme of the app depending on the selected checkbox.
+// DONE: Change the theme of the app depending on the selected checkbox.
 
 (() => {
   const themeSwitcher = () => {
@@ -51,6 +51,22 @@
       const resetBtn = document.querySelector('#button--reset');
       const equalBtn = document.querySelector('#button--equal');
 
+      // NOTE: Tried to add hover effect on buttons
+      // buttonsArr.forEach((item) => {
+      //   const btnColor = window
+      //     .getComputedStyle(item, null)
+      //     .getPropertyValue('background-color');
+      //   console.log(btnColor);
+
+      //   item.addEventListener('mouseenter', () => {
+      //     item.style.background = `rgba(${btnColor}, 1)`;
+      //   });
+
+      //   item.addEventListener('mouseout', () => {
+      //     item.style.background = btnColor;
+      //   });
+      // });
+
       if (theme === 0) {
         bodyEl.style.backgroundColor = 'rgb(57, 70, 99)';
         navEl.style.color = 'rgb(255, 255, 255)';
@@ -69,14 +85,18 @@
         buttonsArr.forEach((item) => {
           item.style.background = '#eae3dd';
           item.children[0].style.color = '#434a59';
+          item.children[1].style.background = '#b4a497';
         });
 
         delBtn.style.background = '#637198';
         delBtn.children[0].style.color = '#fff';
+        delBtn.children[1].style.background = '#414e73';
         resetBtn.style.background = delBtn.style.background;
         resetBtn.children[0].style.color = '#fff';
+        resetBtn.children[1].style.background = '#414e73';
         equalBtn.style.background = '#d03f2f';
         equalBtn.children[0].style.color = '#ffffff';
+        equalBtn.children[1].style.background = '#93271a';
       } else if (theme === 1) {
         bodyEl.style.backgroundColor = 'rgb(230, 230, 230)';
         navEl.style.color = 'rgb(54, 54, 43)';
@@ -95,14 +115,18 @@
         buttonsArr.forEach((item) => {
           item.style.background = '#e4e4e1';
           item.children[0].style.color = '#36362c';
+          item.children[1].style.background = '#a69f91';
         });
 
         delBtn.style.background = '#378187';
         delBtn.children[0].style.color = '#fff';
+        delBtn.children[1].style.background = '#1b6066';
         resetBtn.style.background = delBtn.style.background;
         resetBtn.children[0].style.color = '#fff';
+        resetBtn.children[1].style.background = '#1b6066';
         equalBtn.style.background = '#c85403';
         equalBtn.children[0].style.color = '#ffffff';
+        equalBtn.children[1].style.background = '#873901';
       } else {
         bodyEl.style.backgroundColor = 'rgb(23, 6, 42)';
         navEl.style.color = 'rgb(255, 229, 60)';
@@ -121,6 +145,7 @@
         buttonsArr.forEach((item) => {
           item.style.background = '#331c4d';
           item.children[0].style.color = navEl.style.color;
+          item.children[1].style.background = '#881c9e';
         });
 
         delBtn.style.background = '#55077c';
@@ -129,6 +154,7 @@
         resetBtn.children[0].style.color = '#fff';
         equalBtn.style.background = '#04ded0';
         equalBtn.children[0].style.color = '#1a2327';
+        equalBtn.children[1].style.background = '#6df9f1';
       }
     };
 
